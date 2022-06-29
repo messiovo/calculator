e82b063a47e6fd3e887c6ec6c33f50463d21b811
Messiovo <messiovo@gmail.com>
2022-06-29 20:01:20 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Calc from "./component/Calc";
 
 function App() {
   return (
-    <div >
+    <div className="h-full" >
       <div className='bg-teal-900'>
       <h2 className="text-center text-white font-semibold py-10 text-2xl">CALCULATOR</h2>
       </div>
       <Calc/>
-      <div className='bg-teal-900 mt-10 p-10 max-h-full'>
-      <p className='text-center text-white'>&copy; Messiovo</p>
+      <div className='mt-10'>
+      <p className='text-center font-semibold pt-20'>&copy; Messiovo</p>
       </div>
     </div>
   );
