a69fd47f83cd3184c3fa3f664125b65b6523e426
Messiovo <messiovo@gmail.com>
2022-06-29 18:41:23 +0100

    changes to value

--- src/component/Calc.js
+++ src/component/Calc.js
@@ -10,58 +10,58 @@ const Calc = () => {
           <button className='bg-teal-900'>
             C
           </button>
-          <button className='bg-teal-900' >
-            +-
+          <button className='bg-teal-900'value="del" onClick={(e)=> setVal(val + e.target.value)}>
+            del
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900'value="%" onClick={(e) => setVal(val + e.target.value)}>
             %
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900'value="/" onClick={(e)=> setVal(val + e.target.value)}>
             /
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900' value="7" onClick={(e)=> setVal(val + e.target.value)}>
             7
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900'value="8" onClick={(e)=> setVal(val + e.target.value)}>
             8
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900'value="9" onClick={(e)=> setVal(val + e.target.value)}>
             9
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900'value="x" onClick={(e)=> setVal(val + e.target.value)}>
             x
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900'value="4" onClick={(e)=> setVal(val + e.target.value)}>
             4
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900'value="5" onClick={(e)=> setVal(val + e.target.value)}>
             5
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900'value="6" onClick={(e)=> setVal(val + e.target.value)}>
             6
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900'value="-" onClick={(e)=> setVal(val + e.target.value)}>
             -
           </button>
           <button className='bg-teal-900'value="1" onClick={(e)=> setVal(val + e.target.value)}>
             1
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900'value="2" onClick={(e)=> setVal(val + e.target.value)}>
             2
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900' value="3" onClick = {(e) => setVal(val + e.target.value)}>
             3
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900'value="+" onClick={(e) => setVal(val + e.target.value)}>
             +
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900'value="0" onClick={(e)=> setVal(val + e.target.value)}>
             0
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900' value="." onClick={(e)=> setVal(val + e.target.value)}>
             .
           </button>
-          <button className='bg-teal-900'>
+          <button className='bg-teal-900'value="=" onClick={(e) => setVal(val + e.target.value)}>
             =
           </button>
 </div>
